896211dfcb4313bd97da231760f6c886ce2a7b9e
chpark <jenson.park@nexr.com>
2024-11-18 20:58:03 +0900

    update keymap

--- keyboards/ergodox_white/keymaps/default/keymap.c
+++ keyboards/ergodox_white/keymaps/default/keymap.c
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       // left hand
       KC_ESC,             KC_1,     KC_2,     KC_3,     KC_4,    KC_5,  KC_TRNS,            TG(LOST_ARK),     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
       KC_TAB,             KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,  KC_DEL,             KC_QUOTE,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_EQL,
-      LT(SYMB, KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,    KC_G,  KC_GRV,             KC_DOUBLE_QUOTE,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  MO(SYMB),
-      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,                                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-      KC_LCTL,            KC_QUOT,  KC_TRNS,  KC_LALT,  MOD_FUNC,                                               KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,  KC_BSLS,
+      KC_CAPS,            KC_A,     KC_S,     KC_D,     KC_F,    KC_G,  KC_GRV,             KC_DOUBLE_QUOTE,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  MO(SYMB),
+      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,                                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+      KC_LCTL,            KC_SLEP,  KC_TRNS,  KC_LALT,  MO(SYMB),                                             KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_BSLS,
                                                                  KC_DEL,  KC_LGUI,            TG(SYMB),  KC_DOUBLE_QUOTE,
                                                                  KC_HOME,                     KC_PGUP,
                                                                  KC_BSPC, KC_LGUI, MO(SYMB),  KC_PGDN,   KC_ENT, KC_SPC
@@ -135,11 +135,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [SYMB] = LAYOUT(
           // left hand
-          KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,      KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-          KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,      KC_TRNS, KC_MINS, KC_EQL,  KC_UP,   KC_BSLS, KC_ASTR, KC_F12,
-          KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_TRNS,      KC_DOWN, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_SLSH, KC_TRNS,
-          KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                        KC_AMPR, C(KC_A), KC_PGUP, C(KC_E), KC_BSLS, KC_TRNS,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                          KC_TRNS, KC_PGDN, KC_TRNS, KC_EQL,  KC_TRNS,
+          KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,      KC_TRNS, KC_F6,   KC_F7,      KC_F8,    KC_F9,      KC_F10,       KC_F11,
+          KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,      KC_TRNS, KC_MINS, KC_EQL,     KC_UP,    KC_BSLS,    KC_ASTR,      KC_F12,
+          KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_TRNS,      KC_DOWN, KC_TRNS, KC_LEFT,    KC_DOWN,  KC_RGHT,    KC_SLSH,      KC_PGUP,
+          KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                        KC_AMPR, C(KC_A),    KC_PGUP,  C(KC_E),    KC_BSLS,      KC_PGDN,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                          C(KC_LEFT), C(KC_UP), C(KC_DOWN), C(KC_RIGHT),  KC_TRNS,
                                                        RGB_MOD, KC_TRNS,      TG(SYMB), RGB_M_P,
                                                                 KC_TRNS,      KC_TRNS,
                                               RGB_VAD, RGB_VAI, KC_TRNS,      KC_TRNS, RGB_HUD, RGB_HUI
